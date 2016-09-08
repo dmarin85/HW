3c20ecee84916f73ce1f4eb6a5c7d1180643f329
dmarin85 <didiermarin@att.net>
2016-09-08 12:32:12 -0500

    Add files via upload

---
+++ HelloWorldService/enums/HelloWorldWriterTypes.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HelloWorldService
+{
+	/// <summary>
+	/// Unknow should not exists. It is here to demonstrate a unit test case
+	/// </summary>
+	public enum HelloWorldWriterType
+	{
+		Unknown,
+		Console,
+		Database
+	}
+}
