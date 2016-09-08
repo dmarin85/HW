b5222e1a7621096fa4334346a906e8100b4ad1bb
dmarin85 <didiermarin@att.net>
2016-09-08 12:23:54 -0500

    Add files via upload

---
+++ HelloServices.Tests/HelloWorldFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HelloWorldService;
+using HelloWorldWriter;
+using NUnit.Framework;
+
+namespace HelloServices.Tests
+{
+	/// <summary>
+	/// These tests have no real values. They just demonstrate the ability to write unit test
+	/// </summary>
+	[TestFixture]
+    public class HelloWorldFactoryTests
+    {
+		[Test]
+		public void GetInstance_Console_ReturnsAConsoleHelloWorldWriter()
+		{
+			IHelloWorldWriter writer = null;
+
+			writer = HelloWorldWriterFactory.GetInstance(HelloWorldWriterType.Console);
+
+			Assert.IsTrue(writer is IHelloWorldWriter);
+			Assert.IsTrue(writer is ConsoleHelloWorldWriter);
+			Assert.IsFalse(writer is DataBaseHelloWorldWriter);
+		}
+
+		[Test]
+		public void GetInstance_Unkbown_ThrowsAnNotImplementedException()
+		{
+			Assert.Throws<NotImplementedException>(() => HelloWorldWriterFactory.GetInstance(HelloWorldWriterType.Unknown));
+		}
+    }
+}
